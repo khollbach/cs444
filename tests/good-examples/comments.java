06dbf8621146189c253cf6f192546896ea2e70fa
Kevan Hollbach <khollbach@gmail.com>
2021-02-04 17:20:04 -0500

    Add tokenizer test cases

---
+++ tests/good-examples/comments.java
@@ -0,0 +1,24 @@
+/** Testing comments */
+public /* asdf */ class A {
+    /**
+     * Some docs here...
+     *
+     * more docs..
+     *
+     */
+    public static void run() {
+        String /**/ hw = "Hello, world!\n" //;
+        ;
+        A.print_string(hw);
+    }
+
+    public static void run() {
+        /*
+        String hw = "Hello, world!\n";
+        A.print_string(hw); */
+    }
+
+    static void print_string(String s) {
+        // Code to print the string ...
+    }
+}
---
+++ tests/good-examples/hello-world.java
@@ -0,0 +1,10 @@
+class A {
+    static void run() {
+        String hw = "Hello, world!\n";
+        A.print_string(hw);
+    }
+
+    static void print_string(String s) {
+        // Code to print the string ...
+    }
+}
